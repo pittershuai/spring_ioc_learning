e31c40e16ae609ecc37a80e34bd3bd4bb8695173
root <123>
2019-04-08 16:19:58 +0800

    Bean的settere方法的属性注入——注入对象属性

---
+++ src/main/java/com/bupt/ioc/demo4/Cat.java
@@ -0,0 +1,18 @@
+package com.bupt.ioc.demo4;
+
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "name:"+ name;
+    }
+}
--- src/main/java/com/bupt/ioc/demo4/Person.java
+++ src/main/java/com/bupt/ioc/demo4/Person.java
@@ -3,6 +3,15 @@ package com.bupt.ioc.demo4;
 public class Person {
     private String name;
     private String age;
+    private Cat cat;
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
 
     public String getName() {
         return name;
@@ -22,6 +31,6 @@ public class Person {
 
     @Override
     public String toString() {
-        return "name:" + name +"  "+ "age:"+age;
+        return "name:" + name +"  "+ "age:"+age + "  " +"cat" + cat;
     }
 }
