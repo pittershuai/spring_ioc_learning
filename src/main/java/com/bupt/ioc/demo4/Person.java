a6f00c22922d293ba97713776b95b816e910eb14
root <123>
2019-04-08 16:15:13 +0800

    Bean的settere方法的属性注入

---
+++ src/main/java/com/bupt/ioc/demo4/Person.java
@@ -0,0 +1,27 @@
+package com.bupt.ioc.demo4;
+
+public class Person {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "name:" + name +"  "+ "age:"+age;
+    }
+}
--- src/main/java/com/bupt/ioc/demo4/SpringDemo4.java
+++ src/main/java/com/bupt/ioc/demo4/SpringDemo4.java
@@ -1,6 +1,5 @@
 package com.bupt.ioc.demo4;
 
-import com.bupt.ioc.demo3.Person;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,5 +10,12 @@ public class SpringDemo4 {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         User user = (User)applicationContext.getBean("user");
         System.out.println(user);
+    }
+
+    @Test
+    public void demo2(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = (Person)applicationContext.getBean("person");
+        System.out.println(person);
     }
 }
