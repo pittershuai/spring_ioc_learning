b5e258ec67f9d73e93f77758c51b843077d15852
root <123>
2019-04-09 16:13:17 +0800

    aspectJ后置通知

--- src/main/java/com/bupt/aspectJ/demo1/MyAspectAnno.java
+++ src/main/java/com/bupt/aspectJ/demo1/MyAspectAnno.java
@@ -1,6 +1,7 @@
 package com.bupt.aspectJ.demo1;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -9,9 +10,15 @@ public class MyAspectAnno {
     //只对ProductDao类的save方法进行拦截
 //    @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.save(..))")
     //对所有ProductDao类的所有方法进行拦截
-    @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.*(..))")
+    @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.save(..))")
 //    传入JointPoint获取切点信息
     public void  before(JoinPoint joinPoint){
         System.out.println("前置通知==================" + joinPoint);
+    }
+
+    @AfterReturning(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.update(..))",returning = "result")
+//    后置通知可以得到切入点的返回值
+    public void  after(Object result){
+        System.out.println("后置通知==================" + result);
     }
 }
