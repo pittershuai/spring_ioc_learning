6ce1fbbf6b35584b01c1aa231914e2af1a7e6ee8
root <123>
2019-04-09 16:02:23 +0800

    传入JointPoint获取切点信息

--- src/main/java/com/bupt/aspectJ/demo1/MyAspectAnno.java
+++ src/main/java/com/bupt/aspectJ/demo1/MyAspectAnno.java
@@ -1,5 +1,6 @@
 package com.bupt.aspectJ.demo1;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -9,7 +10,8 @@ public class MyAspectAnno {
 //    @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.save(..))")
     //对所有ProductDao类的所有方法进行拦截
     @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.*(..))")
-    public void  before(){
-        System.out.println("前置通知==================");
+//    传入JointPoint获取切点信息
+    public void  before(JoinPoint joinPoint){
+        System.out.println("前置通知==================" + joinPoint);
     }
 }
