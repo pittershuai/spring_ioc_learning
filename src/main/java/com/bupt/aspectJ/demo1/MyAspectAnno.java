8d33aad7c7e53b5d55855e96954593d10c07c40e
root <123>
2019-04-09 17:48:39 +0800

    aspectJ @Pointcut为切点命名

--- src/main/java/com/bupt/aspectJ/demo1/MyAspectAnno.java
+++ src/main/java/com/bupt/aspectJ/demo1/MyAspectAnno.java
@@ -7,37 +7,47 @@ import org.junit.Test;
 
 @Aspect
 public class MyAspectAnno {
-    //只对ProductDao类的save方法进行拦截
-//    @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.save(..))")
-    //对所有ProductDao类的所有方法进行拦截
-    @Before(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.save(..))")
-//    传入JointPoint获取切点信息
-    public void  before(JoinPoint joinPoint){
-        System.out.println("前置通知==================" + joinPoint);
+    @Before(value="myPointcut1()")
+    public void before(JoinPoint joinPoint){
+        System.out.println("前置通知=================="+joinPoint);
     }
 
-    @AfterReturning(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.update(..))",returning = "result")
-//    后置通知可以得到切入点的返回值
-    public void  after(Object result){
-        System.out.println("后置通知==================" + result);
+    @AfterReturning(value="myPointcut2()",returning = "result")
+    public void afterReturing(Object result){
+        System.out.println("后置通知=================="+result);
     }
 
-    @Around(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.delete(..))")
+    @Around(value="myPointcut3()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("环绕通知前==================");
-        Object obj = joinPoint.proceed();// 执行目标方法,若不写这句delete()是不会被执行的
-        System.out.println("环绕通知后==================");
+        System.out.println("环绕前通知================");
+        Object obj = joinPoint.proceed(); // 执行目标方法
+        System.out.println("环绕后通知================");
         return obj;
     }
 
-    @AfterThrowing(value = "execution(* com.bupt.aspectJ.demo1.ProductDao.findOne(..))",throwing = "e")
+    @AfterThrowing(value="myPointcut4()",throwing = "e")
     public void afterThrowing(Throwable e){
         System.out.println("异常抛出通知=============="+e.getMessage());
     }
 
-    @After(value="execution(* com.bupt.aspectJ.demo1.ProductDao.findAll(..))")
-    //无论是否抛异常都会执行
-    public void after2(){
+    @After(value="myPointcut5()")
+    public void after(){
         System.out.println("最终通知==================");
     }
+
+
+    @Pointcut(value="execution(* com.bupt.aspectJ.demo1.ProductDao.save(..))")
+    private void myPointcut1(){}
+
+    @Pointcut(value="execution(* com.bupt.aspectJ.demo1.ProductDao.update(..))")
+    private void myPointcut2(){}
+
+    @Pointcut(value="execution(* com.bupt.aspectJ.demo1.ProductDao.delete(..))")
+    private void myPointcut3(){}
+
+    @Pointcut(value="execution(* com.bupt.aspectJ.demo1.ProductDao.findOne(..))")
+    private void myPointcut4(){}
+
+    @Pointcut(value="execution(* com.bupt.aspectJ.demo1.ProductDao.findAll(..))")
+    private void myPointcut5(){}
 }
